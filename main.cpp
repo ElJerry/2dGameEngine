9eddac2912e3a4c6dfc81a851b68cc2a3de877c9
ElJerry <kanegerardo_@hotmail.com>
2019-01-01 18:51:55 -0600

    Beggining of testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+extern "C"
+{
+
+#include <SDL.h>
+}
 
 using namespace std;
 
 int main(){
 	cout << "--- 2D Game Engine ---";
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *window = SDL_CreateWindow("Title",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+	return 0;
 }
